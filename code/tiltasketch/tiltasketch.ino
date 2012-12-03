2c200043382e36215cc23a2d5b6ac12070b3b6ac
Rick Anderson <rianders@docs.rutgers.edu>
2012-12-03 11:19:22 -0500

    Initial project frame work added. Non functional state.

---
+++ code/tiltasketch/tiltasketch.ino
@@ -0,0 +1,29 @@
+/***************************
+Tilt-a-sketch is a Arduino sketch that uses two tilt sensors to draw
+on a graphic LCD like an etch-a-sketch lets you turn a knob.
+
+In this code you are essential drawing with up, down, left, right in an
+of or on state.
+
+Latest Version of Code:
+https://github.com/ricklon/tiltasketch
+
+By Rick Anderson
+Apache Version 2.0 licence
+http://www.apache.org/licenses/LICENSE-2.0
+*****************************/
+
+//TODO: update code
+
+
+void setup()
+{
+  
+}
+
+void loop() 
+{
+  
+}
+
+
